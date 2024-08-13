2e4c14b758b1944aebbba2858d0e77b34d458eea
SSroot-Linux <hacizadehiman10@gmail.com>
2024-08-13 15:20:14 +0400

    Update İnfo.py
    Koda Ns Lookup yani Istediğiniz sitenin ip adresini gösteriyor sizin Ip adresinizi Gösterirkende Internet baglantinizin olup olmadığını kontrol ediyor

--- İnfo.py
+++ İnfo.py
@@ -1,3 +1,5 @@
+#pylint:disable=E0001
+#pylint:disable=E0001
 import platform
 import psutil
 import cpuinfo
@@ -6,19 +8,34 @@ import socket
 import time
 import pyfiglet
 import requests
+import webbrowser
+from bs4 import BeautifulSoup
 
 isim = pyfiglet.figlet_format("***MR GESTALT***")
+time.sleep(1)
 print(isim)
-
+time.sleep(1)
 print("Devices Infos")
+time.sleep(1)
 print("---------------------------------------------")
+time.sleep(1)
 print("1-)Bilgileri Göster")
-print("2-)Koddan cık")
+time.sleep(1)
+print("-" * 45)
+time.sleep(1)
+print("2-)Site Ip Sorgu")
+print("-" * 45)
+time.sleep(1)
+print("3-)Koddan cık")
+time.sleep(1)
+print("-" * 45)
 
 
 def sistem_bilgileri():
+    time.sleep(1)
     print("===== İşletim Sistemi Bilgileri =====")
     os_bilgileri = platform.uname()
+    time.sleep(1)
     print(f"Sistem: {os_bilgileri.system}")
     print(f"Node Adı: {os_bilgileri.node}")
     print(f"Sürüm: {os_bilgileri.release}")
@@ -29,6 +46,7 @@ def sistem_bilgileri():
 
     print("\n===== CPU Bilgileri =====")
     cpu_bilgileri = cpuinfo.get_cpu_info()
+    time.sleep(1)
     print(f"CPU Marka ve Model: {cpu_bilgileri['brand_raw']}")
     print(f"Arch: {cpu_bilgileri['arch']}")
     print(f"Bits: {cpu_bilgileri['bits']}")
@@ -37,28 +55,48 @@ def sistem_bilgileri():
     print("\n===== Ağ Bilgileri =====")
     host_adı = socket.gethostname()
     ip_adresi = socket.gethostbyname(host_adı)
+    time.sleep(1)
     print(f"Host Adı: {host_adı}")
-    api = "https://ipinfo.io/json"
-    istek = requests.get(api)
-    data = istek.json()
-    if istek.status_code == 200:
-       print("Internet Baglantisi Basarili")
-       print(f"Ip Adresi: {data['ip']}")
-       print(f"Local Host IP Adresi: {ip_adresi}")
-       
-
+    try:
+        api = "https://ipinfo.io/json"
+        istek = requests.get(api)
+        data = istek.json()
+        print("Internet Bağlantısı Başarılı")
+        print(f"IP Adresi: {data['ip']}")
+        print(f"Local Host IP Adresi: {ip_adresi}")
+    except requests.exceptions.RequestException:
+        print("Internet bağlantısı başarısız. Lütfen internete bağlanın.")
+        
+def site():
+   site = input("Site Isim: ") 
+   url = f"https://www.ipsorgu.com/site_ip_adresi_sorgulama.php?site={site}#sorgu_sonuc"
+   istek2 = requests.get(url)
+   if istek2.status_code == 200:
+      cekme = BeautifulSoup(istek2.text, "html.parser")    
+      baslik = cekme.title.string
+      print(baslik)
+   else:
+      print("Istek basarisiz oldu")                             
 while True:
    try:
-      choise = input("Secimin: ")
+      choise = input("Seçimin: ")
+      time.sleep(1)
+      print("-" * 45)
+      time.sleep(1)
+      print("\n")
       if choise == "1":
          time.sleep(1)
          sistem_bilgileri()
+         time.sleep(3)
+         print("-" * 45)
          time.sleep(1)
-         secim = input("Entere bas ")
-         if secim == "":
-            break
+         print("\n")
       elif choise == "2":
+         time.sleep(1)
+         site()
+         time.sleep(1)
+      elif choise == "3":
          print("Tamam")
          break
-   except:
-      print("HATA")      
+   except Exception as e:
+      print(f"Hata:{e}")
